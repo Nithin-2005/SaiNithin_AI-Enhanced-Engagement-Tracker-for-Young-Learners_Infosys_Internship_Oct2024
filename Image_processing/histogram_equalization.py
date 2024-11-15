4f25f11a1e01d8c2196687fca724857f6d24d070
Sai Nithin Talasu <137865345+Nithin-2005@users.noreply.github.com>
2024-11-15 23:11:29 +0530

    Delete Image_processing directory

--- Image_processing/histogram_equalization.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img = cv2.imread("C:/Users/nithi/OneDrive/Desktop/Infosys Springboard/Infosys Springboard/Image_processing/image1.jpg", 0)
-equalized = cv2.equalizeHist(img)
-
-cv2.imshow('Equalized Image', equalized)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
